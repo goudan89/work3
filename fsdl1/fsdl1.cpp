a64895f1ac1d88829065dd80dd86aad0fa87871b
goudan89 <1046869217@qq.com>
2020-08-01 02:40:15 +0800

    week 3

---
+++ fsdl1/fsdl1.cpp
@@ -0,0 +1,230 @@
+﻿// fsdl1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#define __STDC_CONSTANT_MACROS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+
+extern "C"
+{
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+#include "SDL2/SDL.h"
+};
+#pragma comment(lib,"legacy_stdio_definitions.lib")
+extern "C" { FILE __iob_func[3] = { *stdin,*stdout,*stderr }; }
+const int bpp = 12;
+
+int screen_w = 500, screen_h = 500;
+const int pixel_w = 320, pixel_h = 180;
+
+unsigned char buffer[pixel_w * pixel_h * bpp / 8];
+
+//Refresh Event
+#define REFRESH_EVENT  (SDL_USEREVENT + 1)
+//Break
+#define BREAK_EVENT  (SDL_USEREVENT + 2)
+
+int thread_exit = 0;
+
+int refresh_video(void* opaque) {
+	thread_exit = 0;
+	while (thread_exit == 0) {
+		SDL_Event event;
+		event.type = REFRESH_EVENT;
+		SDL_PushEvent(&event);
+		SDL_Delay(40);
+	}
+	thread_exit = 0;
+	//Break
+	SDL_Event event;
+	event.type = BREAK_EVENT;
+	SDL_PushEvent(&event);
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	AVFormatContext* pFormatCtx;
+	int				i, videoindex;
+	AVCodecContext* pCodecCtx;
+	AVCodec* pCodec;
+	AVFrame* pFrame, * pFrameYUV;
+	uint8_t* out_buffer;
+	AVPacket* packet;
+	int y_size;
+	int ret, got_picture;
+	struct SwsContext* img_convert_ctx;
+	//输入文件路径
+	//char filepath[] = "2018.mp4";
+	char filepath[500] = { 0 };
+	strcpy(filepath, argv[1]);
+	int frame_cnt;
+
+	av_register_all();
+	avformat_network_init();
+	pFormatCtx = avformat_alloc_context();
+
+	if (avformat_open_input(&pFormatCtx, filepath, NULL, NULL) != 0) {
+		printf("Couldn't open input stream.\n");
+		return -1;
+	}
+	if (avformat_find_stream_info(pFormatCtx, NULL) < 0) {
+		printf("Couldn't find stream information.\n");
+		return -1;
+	}
+	videoindex = -1;
+	for (i = 0; i < pFormatCtx->nb_streams; i++)
+		if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
+			videoindex = i;
+			break;
+		}
+	if (videoindex == -1) {
+		printf("Didn't find a video stream.\n");
+		return -1;
+	}
+
+	pCodecCtx = pFormatCtx->streams[videoindex]->codec;
+	pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
+	if (pCodec == NULL) {
+		printf("Codec not found.\n");
+		return -1;
+	}
+	if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+		printf("Could not open codec.\n");
+		return -1;
+	}
+	/*
+	 * 在此处添加输出视频信息的代码
+	 * 取自于pFormatCtx，使用fprintf()
+	 */
+	pFrame = av_frame_alloc();
+	pFrameYUV = av_frame_alloc();
+	out_buffer = (uint8_t*)av_malloc(avpicture_get_size(PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height));
+	avpicture_fill((AVPicture*)pFrameYUV, out_buffer, PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height);
+	packet = (AVPacket*)av_malloc(sizeof(AVPacket));
+	//Output Info-----------------------------
+	printf("--------------- File Information ----------------\n");
+	av_dump_format(pFormatCtx, 0, filepath, 0);
+	printf("-------------------------------------------------\n");
+	img_convert_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt,
+		pCodecCtx->width, pCodecCtx->height, PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL);
+
+	frame_cnt = 0;
+	//放sdl
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		printf("Could not initialize SDL - %s\n", SDL_GetError());
+		return -1;
+	}
+
+	SDL_Window* screen;
+
+	//SDL 2.0 Support for multiple windows
+	screen_w = pCodecCtx->width;
+	screen_h = pCodecCtx->height;
+	screen = SDL_CreateWindow("Simplest ffmpeg player's Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		screen_w, screen_h, SDL_WINDOW_OPENGL);
+	if (!screen) {
+		printf("SDL: could not create window - exiting:%s\n", SDL_GetError());
+		return -1;
+	}
+	SDL_Renderer* sdlRenderer = SDL_CreateRenderer(screen, -1, 0);
+
+	Uint32 pixformat = 0;
+	//IYUV: Y + U + V  (3 planes)
+	//YV12: Y + V + U  (3 planes)
+	pixformat = SDL_PIXELFORMAT_IYUV;
+
+	SDL_Texture* sdlTexture = SDL_CreateTexture(sdlRenderer, pixformat, SDL_TEXTUREACCESS_STREAMING, pCodecCtx->width, pCodecCtx->height);
+ //不再读文件，直接让yuv显示
+	SDL_Rect sdlRect;
+
+	SDL_Thread* refresh_thread = SDL_CreateThread(refresh_video, NULL, NULL);
+	SDL_Event event;
+
+	while(1) {
+		//多线程
+		SDL_WaitEvent(&event);
+		if (event.type == REFRESH_EVENT) {
+			while (1)
+			{//只有视频文件（加音频会很卡
+				if (av_read_frame(pFormatCtx, packet) < 0)
+				{
+					thread_exit = 1;
+				}
+				if (packet->stream_index == videoindex)
+					break;
+			}
+					/*
+					 * 在此处添加输出H264码流的代码
+					 * 取自于packet，使用fwrite()
+					 */
+					ret = avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, packet);
+					if (ret < 0) {
+						printf("Decode Error.\n");
+						return -1;
+					}
+					if (got_picture) {
+						sws_scale(img_convert_ctx, (const uint8_t * const*)pFrame->data, pFrame->linesize, 0, pCodecCtx->height,
+							pFrameYUV->data, pFrameYUV->linesize);
+					//	printf("Decoded frame index: %d\n", frame_cnt);
+
+						/*
+						 * 在此处添加输出YUV的代码
+						 * 取自于pFrameYUV
+						 */
+
+						 //显示
+						SDL_UpdateTexture(sdlTexture, NULL, pFrameYUV->data[0], pFrameYUV->linesize[0]);
+
+						//FIX: If window is resize
+						sdlRect.x = 0;
+						sdlRect.y = 0;
+						sdlRect.w = screen_w;
+						sdlRect.h = screen_h;
+
+						SDL_RenderClear(sdlRenderer);
+						SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, &sdlRect);
+						SDL_RenderPresent(sdlRenderer);
+
+
+						frame_cnt++;
+
+					}
+				
+				av_free_packet(packet);
+				//SDL_Delay(40);
+			
+
+		}
+
+		else if (event.type == SDL_QUIT) {
+			thread_exit = 1;
+		}
+		else if (event.type == BREAK_EVENT) {
+			break;
+		}
+
+	}
+	sws_freeContext(img_convert_ctx);
+	SDL_Quit();
+	av_frame_free(&pFrameYUV);
+	av_frame_free(&pFrame);
+	avcodec_close(pCodecCtx);
+	avformat_close_input(&pFormatCtx);
+
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
